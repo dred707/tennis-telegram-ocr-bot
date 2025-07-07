3f2578ff0c080a1625f3410874102c6cc985f241
Ihor Khomin <dred707@gmail.com>
2025-07-08 02:14:03 +0300

    - delete_webhook

--- main.py
+++ main.py
@@ -135,7 +135,13 @@ async def run_webhook():
 
 async def run_polling():
     print("🖥 Запуск у polling-режимі")
+
     app = await build_application()
+
+    print("🧼 Знімаю webhook...")
+    await app.bot.delete_webhook(drop_pending_updates=True)
+
+    print("🚀 Запуск Application (polling)...")
     await app.run_polling()
 
 
